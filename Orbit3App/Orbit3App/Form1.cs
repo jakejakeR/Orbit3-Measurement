bfcaf12e0ab1937a2978bcb591237e2814e3a393
Jakub Rogowski <jakerogowski33@gmail.com>
2022-06-13 16:38:54 +0200

    Initialising the OrbitServer

--- Orbit3App/Orbit3App/Form1.cs
+++ Orbit3App/Orbit3App/Form1.cs
@@ -4,9 +4,11 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Solartron.Orbit3;
 
 namespace Orbit3App
 {
@@ -14,7 +16,19 @@ namespace Orbit3App
     {
         public Form1()
         {
+            OrbitServer Orbit;
+
+            try
+            {
+                Orbit = new OrbitServer();
+            } catch(Exception Ex)
+            {
+                MessageBox.Show(Ex.Message);
+                Environment.Exit(0); // Close app if there's an init failure
+            }
+
             InitializeComponent();
+            this.Text = "Orbit3 Measurement App V" + Assembly.GetExecutingAssembly().GetName().Version.ToString();
         }
     }
 }
