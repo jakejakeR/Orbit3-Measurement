329fee0ba4f84b1718a6db73aaee1a8eb29c8384
Jakub Rogowski <jakerogowski33@gmail.com>
2022-06-20 13:09:54 +0200

    Module class added

---
+++ Orbit3App/Orbit3App/Module.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orbit3App
+{
+    internal class Module
+    {
+        public String Name { get; set; }
+        public double[] Reads { get; set; }
+        private int CollectionSize;
+
+        public Module(int CollectionSize)
+        {
+            this.CollectionSize = CollectionSize;
+            Reads = new double[CollectionSize];
+        }
+    }
+}
